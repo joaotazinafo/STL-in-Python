91ad1a09a12d3b99cc581843458f405117f5e757
João Matheus Lopes Tazinafo <joaomatheuslt@gmail.com>
2022-03-15 11:07:56 -0300

    Adicionei o código em python

---
+++ Cubo.py
@@ -0,0 +1,36 @@
+import numpy as np
+from stl import mesh
+
+# Definir os vertices
+vertices = np.array([\
+    [-1, -1, -1],
+    [+1, -1, -1],
+    [+1, +1, -1],
+    [-1, +1, -1],
+    [-1, -1, +1],
+    [+1, -1, +1],
+    [+1, +1, +1],
+    [-1, +1, +1]])
+# Define os triangulos para a composição das faces do objeto
+faces = np.array([\
+    [0,3,1],
+    [1,3,2],
+    [0,4,7],
+    [0,7,3],
+    [4,5,6],
+    [4,6,7],
+    [5,1,2],
+    [5,2,6],
+    [2,3,6],
+    [3,7,6],
+    [0,1,5],
+    [0,5,4]])
+
+# Cria um Mesh
+cube = mesh.Mesh(np.zeros(faces.shape[0], dtype=mesh.Mesh.dtype))
+for i, f in enumerate(faces):
+    for j in range(3):
+        cube.vectors[i][j] = vertices[f[j],:]
+
+# Cria o arquivo mesh com o nomde de sua preferencia, e com o tipo STL
+cube.save('cube.stl')
